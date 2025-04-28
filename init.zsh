0298cc7e85963464306aff12312892ef3324189b
Jason L Connor <jason.connor@gmail.com>
2025-04-28 15:11:54 -0600

    Homebrew initialization

--- init.zsh
+++ init.zsh
@@ -1,4 +1,17 @@
-# This is example code.
-# This will be executed when this module is initialized during Zsh startup.
-
-print "executed code in ${0}"
+if (( ! ${+HOMEBREW_PREFIX} )); then
+  () {
+    local prefix cmd
+    for prefix in /opt/homebrew /usr/local /home/linuxbrew/.linuxbrew; do
+      if [[ -e ${prefix}/bin/brew ]]; then
+        cmd=${prefix}/bin/brew
+        break
+      fi
+    done
+    if [[ -z ${cmd} ]] return 1
+    if [[ ! ( -s ${1} && ${1} -nt ${cmd} ) ]]; then
+      ${cmd} shellenv >! ${1} || return 1
+      zcompile -UR ${1}
+    fi
+    source ${1}
+  } ${0:h}/brew-shellenv.zsh || return 1
+fi
